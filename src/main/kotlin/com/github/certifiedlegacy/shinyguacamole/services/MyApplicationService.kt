7eff5bb64af9379bc259e86b8b4fa0331632a8fd
GitHub Action <action@github.com>
2020-10-29 00:25:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/certifiedlegacy/shinyguacamole/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.certifiedlegacy.shinyguacamole.services
+
+import com.github.certifiedlegacy.shinyguacamole.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
